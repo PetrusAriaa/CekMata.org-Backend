a81a56832c6d84674a26afc8d28bb2cf034cc753
PetrusAriaa <petrusariacr25@gmail.com>
2024-03-01 15:56:47 +0700

    feat: create db connection

---
+++ db.py
@@ -0,0 +1,21 @@
+from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+load_dotenv(".env.development") # this load_dotenv is necessary
+
+engine = create_engine(getenv('PG_CONNECTION'))
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
