37362bd7981d6ca7a79eb01ed1826f04db88e1da
PetrusAriaa <petrusariacr25@gmail.com>
2024-03-02 11:38:17 +0700

    feat: create ORM for database scheme

--- models/patient.py
+++ models/patient.py
@@ -3,13 +3,16 @@ from sqlalchemy.sql import func
 
 from db import Base
 
-class Users(Base):
-    __tablename__ = "user"
+class Patients(Base):
+    __tablename__ = "patient"
 
     id = Column(UUID, primary_key=True, unique=True)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=text("NULL ON UPDATE CURRENT_TIMESTAMP"))
     deleted_at = Column(DateTime, server_default=text("NULL ON DELETE CURRENT_TIMESTAMP"))
-    username = Column(String(32))
-    password = Column(String(255))
-    last_login = Column(DateTime, nullable=True)
+    code = Column(String(9))
+    name = Column(String(255))
+    birth = Column(Date)
+    phone = Column(String(20))
+    classification = Column(String(255), nullable=True)
+    recommendation = Column(String(255), nullable=True)
