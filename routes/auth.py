1839a11100af8765a3a82546786b49b8ac3e0232
PetrusAriaa <petrusariacr25@gmail.com>
2024-05-12 15:50:14 +0700

    refactor: token response model from dto

--- routes/auth.py
+++ routes/auth.py
@@ -1,17 +1,15 @@
+import bcrypt
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.responses import JSONResponse
 from typing import Annotated, Union
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime, timedelta
 from os import getenv
 from jose import jwt, JWTError
-from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    username: str
+from db import get_db
+from dto import TokenResponseModel
+from models import Users
 
 
 ADMIN_DATA = {
@@ -35,6 +33,11 @@ def __generate_token(data: dict, expires_delta: Union[timedelta, None] = None):
     return token
 
 
+def __validate_password(password: str, true_password:str) -> bool:
+    b_password = password.encode(encoding='utf-8')
+    return bcrypt.checkpw(b_password, bytes(true_password, encoding='utf-8'))
+
+
 def validate_token(token: Annotated[str, Depends(oauth2_token_scheme)]):
     credential_error = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -48,15 +51,22 @@ def validate_token(token: Annotated[str, Depends(oauth2_token_scheme)]):
         raise credential_error
 
 
-@auth_router.post("/login", response_model=Token)
-def local_login(auth_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    if auth_data.username != ADMIN_DATA["USERNAME"]:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not exists", headers={'WWW-Authenticate':'Bearer'})
+@auth_router.post("/login", response_model=TokenResponseModel, status_code=status.HTTP_200_OK)
+def local_login(auth_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session=Depends(get_db))  -> TokenResponseModel:
+    user = db.query(Users).filter(Users.username==auth_data.username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User not found",
+                            headers={'WWW-Authenticate': 'Bearer'})
 
-    if auth_data.password != ADMIN_DATA["PASSWORD"]:
+    if not __validate_password(auth_data.password, user.password):
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Wrong username or password", headers={'WWW-Authenticate':'Bearer'})
     
     token_expire = timedelta(minutes=float(getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     token = __generate_token(data={"agent": ADMIN_DATA["USERNAME"]}, expires_delta=token_expire)
-    res = JSONResponse({"access_token" : token, "token_type": "bearer", "username": ADMIN_DATA["USERNAME"]})
+    res = TokenResponseModel(
+            access_token = token,
+            token_type = "Bearer",
+            username = user.username
+            )
     return res
