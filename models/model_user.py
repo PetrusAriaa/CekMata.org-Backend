41cb944476b577e69d04eed8de35f6827cdb7aae
PetrusAriaa <petrusariacr25@gmail.com>
2024-05-12 15:49:33 +0700

    db: make update and deleted cols nullable

--- models/model_user.py
+++ models/model_user.py
@@ -8,8 +8,8 @@ class Users(Base):
 
     id = Column(UUID, primary_key=True, unique=True, server_default=text("gen_random_uuid()"))
     created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime)
-    deleted_at = Column(DateTime)
+    updated_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
     username = Column(String(32))
     password = Column(String(255))
     last_login = Column(DateTime, nullable=True)
