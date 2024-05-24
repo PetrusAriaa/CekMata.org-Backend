73cda85a978bd0580a889f4ecaf87441b3295e20
PetrusAriaa <petrusariacr25@gmail.com>
2024-05-24 16:37:18 +0700

    fix: handle error in invalid fileType, unprocessable img

--- routes/predict.py
+++ routes/predict.py
@@ -1,5 +1,5 @@
 import io
-from fastapi import APIRouter, Depends, File, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
 from fastapi.responses import JSONResponse
 from PIL import Image
 import torchvision.transforms as transforms
@@ -8,6 +8,12 @@ import torch
 
 from .auth import validate_token
 
+
+file_type = [
+    'image/png',
+    'image/jpeg',
+    'image/jpg'
+]
 
 predictor_router = APIRouter(tags=['Predictor'], dependencies=[Depends(validate_token)])
 
@@ -24,16 +30,29 @@ def prepare(byte_image):
 
 
 @predictor_router.post('')
-def predict(file: UploadFile = File(...)):
+def predict(file: UploadFile):
+    try:
+        file_type.index(file.content_type)
+    except ValueError :
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Cannot process wrong file format. Please send these type of file format: [jpeg/jpg/png]',
+                            headers={
+                                "Accept": "image/jpeg, image/jpg, image/png"
+                            })
     img = file.file.read()
     tensor = prepare(img)
     
     class_name = 'None'
-    with torch.no_grad():
-        model.eval()
-        output =model(tensor)
-        index = output.data.cpu().numpy().argmax()
-        classes = ['Cataract', 'Diabetic Retinopathy', 'Glaucoma', 'Normal']
-        class_name = classes[index]
-    
-    return JSONResponse({'status': class_name})
+    try:
+        with torch.no_grad():
+            model.eval()
+            output =model(tensor)
+            index = output.data.cpu().numpy().argmax()
+            classes = ['Cataract', 'Diabetic Retinopathy', 'Glaucoma', 'Normal']
+            class_name = classes[index]
+        return JSONResponse({'status': class_name})
+    except:
+        raise HTTPException(status_code=400, detail='Cannot process image data, please use different image',
+                            headers={
+                                "Accept": "image/jpeg, image/jpg, image/png"
+                            })
